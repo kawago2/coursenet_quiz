ac7cf53dbe7a40cca7355b5eda2b8e40ccc8f481
kawago2 <itkawago@gmail.com>
2023-09-16 08:16:22 +0700

    add builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:coursenet_quiz/views/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -19,29 +20,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    var pertanyaan = [
-      'Tempat apa yang akan anda kunjungi?',
-      'Apa warna kesukaan anda?',
-    ];
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Aplikasi Quiz'),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          const Text("Pertanyaan"),
-          ElevatedButton(onPressed: () {}, child: const Text("Jawaban 1")),
-          ElevatedButton(onPressed: () {}, child: const Text("Jawaban 2")),
-          ElevatedButton(onPressed: () {}, child: const Text("Jawaban 3")),
-          ElevatedButton(onPressed: () {}, child: const Text("Jawaban 4")),
-        ],
-      ),
-    );
-  }
-}
